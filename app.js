588553c302ba178af36e9eddd4ba6f8c26f34338
Syed-Amjad <syed.amjad.hashmi@gmail.com>
2024-03-03 20:06:28 +0500

    Assignment for 27th Feb

---
+++ app.js
@@ -0,0 +1,22 @@
+//Data types Assignment
+// String
+var fullName = "Syed Amjad Ali";
+console.log(fullName);
+// Number
+var rollNum = 786;
+console.log(rollNum);
+// Boolean
+var bool = true;
+console.log(bool);
+// null
+var value = null;
+console.log(value);
+// undefined
+var undef = undefined;
+console.log(undef);
+// any
+var an = "This value could be anything";
+console.log(an);
+// Array
+var myArray = ["Amjad"];
+console.log(myArray);
---
+++ app.ts
@@ -0,0 +1,30 @@
+//Data types Assignment
+// String
+
+let fullName = "Syed Amjad Ali";
+console.log( fullName);
+
+// Number
+let rollNum = 786;
+console.log(rollNum);
+
+// Boolean
+let bool: boolean = true;
+console.log(bool);
+
+// null
+let value = null;
+console.log(value);
+
+// undefined
+let undef = undefined;
+console.log(undef);
+
+// any
+let an: any = "This value could be anything";
+console.log(an);
+
+// Array
+let myArray = ["Amjad"];
+console.log(myArray);
+
